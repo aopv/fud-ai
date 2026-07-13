fb5c0958df1c4043d2214c9b6a4c03c6d0fd2121
Apoorv Darshan <ad13dtu@gmail.com>
2026-07-13 17:16:11 +0530

    chore: start next mobile release cycle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.apoorvdarshan.calorietracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 31
-        versionName = "3.0.4"
+        versionCode = 32
+        versionName = "3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
