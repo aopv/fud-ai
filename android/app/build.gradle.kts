2f7b13469c99dc1c1b520cb35bbea4cb9da4feaa
Apoorv Darshan <ad13dtu@gmail.com>
2026-07-21 22:29:05 +0530

    Bump app versions to 6.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.apoorvdarshan.calorietracker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 32
-        versionName = "3.1"
+        versionCode = 33
+        versionName = "6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
