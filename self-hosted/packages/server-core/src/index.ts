9e2edc1f1812aab414a85e11566d6b1bc31ca0f5
Apoorv Darshan <ad13dtu@gmail.com>
2026-08-31 05:40:29 +0530

    feat: add self-hosted web app and encrypted sync

---
+++ self-hosted/packages/server-core/src/index.ts
@@ -0,0 +1,8 @@
+export { createSyncHandler, constantTimeTokenEquals } from "./server.js";
+export type {
+  StoragePullResult,
+  StoragePushResult,
+  SyncServerLimits,
+  SyncServerOptions,
+  SyncStorage,
+} from "./types.js";
